e5783d3e56ff1acf1c689fbb787621fa546edae5
so1ar <so1ar114514@gmail.com>
2024-07-01 15:28:11 +0800

    which-key correct

--- lua/kickstart/plugins/which-key.lua
+++ lua/kickstart/plugins/which-key.lua
@@ -29,12 +29,12 @@ return {
         ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
         ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
         ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>g'] = { name = '{G]it Hunk', _ = 'which_key_ignore' },
+        ['<leader>g'] = { name = '[G]it Hunk', _ = 'which_key_ignore' },
         ['<leader>b'] = { name = '[B]uffer', _ = 'which_key_ignore' },
       }
       -- visual mode
       require('which-key').register({
-        ['<leader>h'] = { 'Git [H]unk' },
+        ['<leader>g'] = { '[G]it Hunk' },
       }, { mode = 'v' })
     end,
   },
