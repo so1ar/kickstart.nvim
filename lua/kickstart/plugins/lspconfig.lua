26ab554930a3d6b3c4487a597c4b796b529ffc6a
so1ar <so1ar114514@gmail.com>
2024-07-14 13:29:45 +0800

    make lsconfig lazy loading on BufReadPre

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -1,8 +1,8 @@
 return {
   { -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
-    -- event = { 'BufReadPre' },
-    ft = { 'markdown', 'lua' },
+    event = { 'BufReadPre' },
+    -- ft = { 'markdown', 'lua' },
     dependencies = {
       -- Automatically install LSPs and related tools to stdpath for Neovim
       { 'williamboman/mason.nvim', config = true }, -- NOTE: Must be loaded before dependants
