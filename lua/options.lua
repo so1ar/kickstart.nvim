c1156494ef1981de1f287183b69a620fcec2855f
so1ar <so1ar114514@gmail.com>
2024-07-17 20:43:13 +0800

    remove some comments

--- lua/options.lua
+++ lua/options.lua
@@ -59,10 +59,4 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
--- Tabstop
--- vim.opt.tabstop = 4
--- vim.opt.shiftwidth = 4
--- vim.opt.softtabstop = 4
--- vim.opt.expandtab = true
-
 -- vim: ts=2 sts=2 sw=2 et
