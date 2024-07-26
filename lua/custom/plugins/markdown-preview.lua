125594707be8859116f0732658e5ad25698d8092
so1ar <so1ar114514@gmail.com>
2024-07-26 15:46:52 +0800

    format

--- lua/custom/plugins/markdown-preview.lua
+++ lua/custom/plugins/markdown-preview.lua
@@ -1,6 +1,6 @@
 return {
   'iamcco/markdown-preview.nvim',
-  build = "cd app && yarn install",
+  build = 'cd app && yarn install',
   ft = 'markdown',
   config = function()
     vim.g.mkdp_filetypes = { 'markdown' }
